b7c7ea50168965f2d25cce4eba5e93beab94c0be
Shubhamverma0007 <Shubhamvermasep@gmail.com>
2019-06-14 10:12:00 +0530

    The one

--- src/main/java/com/mkyong/hashing/App.java
+++ src/main/java/com/mkyong/hashing/App.java
@@ -1,14 +1,17 @@
 package com.mkyong.hashing;
 
 /**
- * Hello world!
- *
+ * Hello word!
  */
-public class App 
+
+
+public class App
 {
     public static void main( String[] args )
     {
         Evenodd ob= new Evenodd();
 System.out.println(ob.checkeven(Integer.parseInt(args[0])));
+System.out.println("ok23");
+
     }
 }
